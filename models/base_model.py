180934e41854bd7593059e067526f785506dbad9
root <vincent.okoth856@gmail.com>
2024-02-09 05:48:35 -0800

    corrected a spelling mistake in __clas__ to __class__

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__clas__":
+                if key != "__class__":
                     setattr(self, key, value)
 
     def save(self):
