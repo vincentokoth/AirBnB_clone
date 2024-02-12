cc1b8352fdd47b10cfdb7580395c0d53e78798fb
root <vincent.okoth856@gmail.com>
2024-02-12 10:20:29 -0800

    A class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances.

---
+++ models/engine/file_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
+import json
+import models
+from models.base_model import BaseModel
+from os.path import exists
+
+
+class FileStorage:
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self, cls=None):
+        """Return a dictionary of instantiated objects in __objects.
+
+        if a cls is specified, returns a dictionary of objects of that type.
+        Otherwise, return the __object dictionary.
+        """
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
+        return self.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id."""
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+
+    def save(self):
+        """Serialize __objects to the JON file __file_path."""
+        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(odict, f)
+
+    def reload(self):
+        """Deserialize the JON file __file_path to __objects, if it exists."""
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                for o in json.load(f).values():
+                    name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(name)(**o))
+        except FileNotFoundError:
+            pass
