996e0e2739f65bebf8255131c4b581ed9297ea6b
root <vincent.okoth856@gmail.com>
2024-02-13 09:14:24 -0800

    The consle.py program contains the entry point of the command interpreter.

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines the HBNB console."""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the AirBnB_clone project command interpreter."""
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(elf, arg):
+        """Exit the program gracefully"""
+        print()
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
